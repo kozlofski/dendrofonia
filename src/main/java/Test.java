845058df10ea31251d586ba7f3a0a415f0b58434
Krzysztof Kozlowski <kozlofski.dev@gmail.com>
2024-04-29 13:52:58 +0200

    Initial commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,19 @@
+import processing.core.PApplet;
+
+public class Test extends PApplet {
+    @Override
+    public void settings() {
+    }
+
+    @Override
+    public void setup() {
+    }
+
+    @Override
+    public void draw() {
+    }
+
+    public static void main(String[] args) {
+        PApplet.main(new String[] {Test.class.getName()});
+    }
+}
